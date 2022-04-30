3d1516d6cb7ba68d898d9c4ac17ab15c67001774
TaborMaciej <104684684+TaborMaciej@users.noreply.github.com>
2022-04-30 16:53:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-﻿#include "Values.h"
+#include "Values.h"
 
 int main()
 {
 	cout << "How many numbers will you input? ";
 	int size;
 	cin >> size;
-	vector<double> numbers(size, 0);
+	vector<double> numbers;
+	double temp = 0.0;
 	for(int i = 0; i < size; i++)
 	{
 		cout << "Input nr. " << i + 1 << ": ";
-		cin >> numbers[i];
+		cin >> temp;
+		numbers.push_back(temp);
 	}
 	cout << "Maximum value: " << Values::Max(numbers) << endl
 		<< "Minimum value: " << Values::Min(numbers) << endl
